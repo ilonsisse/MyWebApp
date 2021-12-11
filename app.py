1b6872036740cc9ef54a8da3227271c60ef6b82e
ilonsisse <spiker238@gmail.com>
2021-12-11 19:32:35 +0300

    Регистрация без исключений

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import psycopg2
 
 app = Flask(__name__)
@@ -15,23 +15,37 @@ cursor = conn.cursor()
 def index():
     return render_template('login.html')
 
-
-@app.route('/login/', methods=['POST'])
+@app.route('/login/', methods=['POST', 'GET'])
 def login():
-    username = request.form.get('username')
-    password = request.form.get('password')
-    cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(username), str(password)))
-    records = list(cursor.fetchall())
-    if username and password:
-        return render_template('account.html', full_name=records[0][1], username=username, password=password)
-    if username == '':
-        return render_template('error_1.html')
-    if password == '':
-        return render_template('error_2.html')
-    if username not in database:
-        return render_template('error_3.html')
+    if request.method == 'POST':
+        if request.form.get("login"):
+            username = request.form.get('username')
+            password = request.form.get('password')
+            cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(username), str(password)))
+            records = list(cursor.fetchall())
 
-    
+            return render_template('account.html', full_name=records[0][1])
+        elif request.form.get("registration"):
+            return redirect("/registration/")
+
+    return render_template('login.html')
+
+
+@app.route('/registration/', methods=['POST', 'GET'])
+def registration():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        login = request.form.get('login')
+        password = request.form.get('password')
+
+        cursor.execute('INSERT INTO service.users (full_name, login, password) VALUES (%s, %s, %s);',
+                       (str(name), str(login), str(password)))
+        conn.commit()
+
+        return redirect('/login/')
+
+    return render_template('registration.html')
+
 
 
 if __name__ == '__main__':
